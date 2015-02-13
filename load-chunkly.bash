9839b65435ef48dc84c3577ad2b1acbb41219278
Andrea Francia <andrea@andreafrancia.it>
2015-02-13 21:41:31 +0100

    chunkly prompt working

--- load-chunkly.bash
+++ load-chunkly.bash
@@ -16,4 +16,9 @@ source awesome-terminal-fonts/patched/SourceCodePro+Powerline+Awesome+Regular.sh
 source "$dir/dotfiles/zsh/plugins/time/time.plugin.zsh"
 source "$dir/dotfiles/zsh/plugins/chunkly/chunkly.plugin.zsh"
 
-RPROMPT='$(chunkly_prompt)'
+CC_WHITE='255'
+CC_DRAW_IN_WHITE="%k%f%F{$CC_WHITE}"
+
+cur_dir=$'%{\e[0;90m%}$(tilde_or_pwd)%{\e[0m%}'
+no_color=$'%{\e[0m%}'
+RPROMPT="$cur_dir $(git_cwd_info) $CC_WRITE_IN_WHITE"'$(chunkly_prompt)'"$no_color"
